80ecfceac9aa25f273432ae1358e1ef713c280c3
Keith <keithsalisbury@gmail.com>
2016-01-12 10:22:42 +0000

    Update plug.ex
    Hey, I know its docs, but wouldn't the compiler would say something like:
    
    `lib/plug.ex:33: warning: variable opts is unused`

--- lib/plug.ex
+++ lib/plug.ex
@@ -30,7 +30,7 @@ defmodule Plug do
 
   Here's an example of a function plug:
 
-      def json_header_plug(conn, opts) do
+      def json_header_plug(conn, _opts) do
         conn |> put_resp_content_type("application/json")
       end
 
